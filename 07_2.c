784b26415a61d062864ea17d89b061360ffaabd1
ChaYeJin1 <cha9599831@daum.net>
2018-11-08 15:49:15 +0900

    Update 07_2.c

---
+++ 07_2.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(int argc, char *argv[]) {
+	int x=10;
+	int y=20;
+	int *ptr;
+	
+	ptr = &x;
+	printf("x = %d\n", *ptr);
+	
+	ptr = &y;
+	printf("y = %d\n", *ptr);
+	
+	
+	ptr = &x;    
+	*ptr=20;
+	printf("x = %d\n", x);
+	
+	
+	return 0;
+}
